59ab93045441b6c367b849d2a4cb5f2e6ec62849
stadolf <stadolf@gmail.com>
2024-07-16 23:53:06 +0200

    favicons
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={clsx("dark", inter.className)}>
+      <title>Welshare Presale Phase I</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <body>
         <main className="flex min-h-screen flex-col p-2">
           <div className="container mx-auto max-w-screen-md">
