4f5a3564e57ca22546dc18d9f7730d495a830908
stadolf <stadolf@gmail.com>
2024-07-16 22:18:06 +0200

    uses google sheets backend directly
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import { siweServer } from "@/utils/siweServer";
-import { NextRequest, NextResponse } from "next/server";
-
-export const GET = async (req: NextRequest) => {
-  const session = await siweServer.getSession();
-  return NextResponse.json(session);
-};
---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,77 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as Google from "@googleapis/sheets";
+import { createHmac, timingSafeEqual } from "node:crypto";
+
+const makeClient = () => {
+  const auth = new Google.auth.JWT({
+    email: process.env.GOOGLE_SHEETS_SERVICE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_SHEETS_SERVICE_CLIENT_PRIVATE_KEY!.replace(
+      /\\n/g,
+      "\n"
+    ),
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  return Google.sheets({ version: "v4", auth });
+};
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.text();
+  const { data } = JSON.parse(body);
+  //const { data } = await req.json();
+  //console.log(body);
+  const personaHeaders = req.headers.get("persona-signature");
+  if (!personaHeaders) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const t = personaHeaders.split(",")[0].split("=")[1];
+  const signatures = personaHeaders
+    .split(" ")
+    .map((pair: string) => pair.split("v1=")[1]);
+  const hmac = createHmac("sha256", process.env.PERSONA_WEBHOOK_SECRET!)
+    .update(`${t}.${body}`)
+    .digest("hex");
+
+  const isVerified = signatures.some((signature) => {
+    return timingSafeEqual(Buffer.from(hmac), Buffer.from(signature));
+  });
+
+  if (!isVerified) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const payload = data.attributes.payload.data;
+
+  const submission = {
+    InquiryId: payload.id,
+    CryptoWallet: payload.attributes.fields.cryptoWalletAddress.value,
+    SelectedCountry: payload.attributes.fields.selectedCountryCode.value,
+    AddressCountry: payload.attributes.fields.addressCountryCode.value,
+    EMail: payload.attributes.fields.emailAddress.value,
+    Referrer: payload.attributes.fields.referrer.value,
+  };
+
+  const SPREADSHEET_ID = process.env.GOOGLE_SHEETS_SPREADSHEET_ID;
+
+  const Sheets = makeClient();
+  const result = await Sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: "Persona!A2:F",
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [
+        [
+          submission.CryptoWallet,
+          submission.InquiryId,
+          submission.AddressCountry,
+          submission.SelectedCountry,
+          submission.EMail,
+          submission.Referrer,
+          new Date().toUTCString(),
+        ],
+      ],
+    },
+  });
+  const response = new NextResponse("ok", { status: 200 });
+  return response;
+};
