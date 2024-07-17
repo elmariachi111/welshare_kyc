d386a18318ceb41355591738d85c9e52651d7932
stadolf <stadolf@gmail.com>
2024-07-18 00:05:43 +0200

    adds geoblocking
    Signed-off-by: stadolf <stadolf@gmail.com>

---
+++ src/app/restricted/page.tsx
@@ -0,0 +1,10 @@
+export default function Restricted() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-16">
+      <p className="text-2xl text-center mt-16 w-2/3">
+        Due to regulatory restrictions, the WEL token is unfortunately not
+        available in your country.
+      </p>
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,39 @@ export const config = {
   matcher: "/",
 };
 
+// https://orpa.princeton.edu/export-controls/sanctioned-countries
+
+const BLOCKED_COUNTRIES = [
+  "CU", // Cuba
+  "IR", // Iran, Islamic Republic Of
+  "KP", // Korea, Democratic People's Republic Of
+  "RU", // Russia
+  "SY", // Syrian Arab Republic
+  "US", // merica
+];
+
+const BLOCKED_REGIONS = [
+  "UA-43", // Crimea
+  "UA-14", // Donetsk
+  "UA-09", // Luhansk
+];
+
+const isOFACBlocked = (request: NextRequest) => {
+  const country = request?.geo?.country ?? "DE";
+  const region = request?.geo?.region ?? undefined;
+
+  return (
+    BLOCKED_COUNTRIES.includes(country) ||
+    (region && BLOCKED_REGIONS.includes(region))
+  );
+};
+
 export function middleware(request: NextRequest) {
+  if (isOFACBlocked(request)) {
+    request.nextUrl.pathname = "/restricted";
+    return NextResponse.rewrite(request.nextUrl);
+  }
+
   const ref = request.nextUrl.searchParams.get("ref");
   if (!ref) {
     return NextResponse.next();
