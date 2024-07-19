dd2a452274e6e1affbf4077a25ef26be98b5ddbd
stadolf <stadolf@gmail.com>
2024-07-20 00:43:01 +0200

    refund notice
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/app/restricted/page.tsx
+++ src/app/restricted/page.tsx
@@ -1,9 +1,13 @@
 export default function Restricted() {
   return (
-    <div className="flex flex-col items-center justify-center gap-16">
+    <div className="flex flex-col items-center justify-center gap-8">
       <p className="text-2xl text-center mt-16 w-2/3">
         Due to regulatory restrictions, the WEL token is unfortunately not
         available in your country.
+      </p>
+      <p className="text-gray-400 sm:w-2/3 text-center">
+        If you have already purchased vouchers, you will be eligible for a
+        refund.
       </p>
     </div>
   );
