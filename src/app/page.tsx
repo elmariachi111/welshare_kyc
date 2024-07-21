99c9b790582cd834d8d42c7c030f3ca2891399f7
stadolf <stadolf@gmail.com>
2024-07-21 21:44:49 +0200

    metadata & social images activates vercel analytics
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import SignedInFlow from "@/components/SignedInFlow";
-import { Metadata } from "next";
 import { cookies } from "next/headers";
-
-export const metadata: Metadata = {
-  title: "Welshare Presale Phase 1",
-  description: "Welshare Presale Phase 1",
-};
 
 export default function Home() {
   const referrer = cookies().get("referrer")?.value;
